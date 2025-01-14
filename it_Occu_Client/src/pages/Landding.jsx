54420be7ea59d744ae8f5c0f05391939cb7e1553
Nattawut-Pond <nattawut.poonmalang@gmail.com>
2025-01-15 01:48:29 +0700

    Refactor Axios import to use ES6 import syntax in Landding component

--- it_Occu_Client/src/pages/Landding.jsx
+++ it_Occu_Client/src/pages/Landding.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-const axios = require('axios');
+import axios from 'axios';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
