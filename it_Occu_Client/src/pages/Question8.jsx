d2795782b5bb9f051f8bf3c07a4ca16bc3383e94
Nattawut-Pond <nattawut.poonmalang@gmail.com>
2025-01-15 12:22:32 +0700

    Add timestamp to form submission in Question8 component

--- it_Occu_Client/src/pages/Question8.jsx
+++ it_Occu_Client/src/pages/Question8.jsx
@@ -57,9 +57,13 @@ function Question8() {
       const averageScore = totalScore / numQuestions;
       const suitabilityPercentage = ((5 - averageScore) / 4) * 100;
 
+      const date = new Date();
+      const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}Z`;
+
       // Store the submission with the current timestamp in the database
       try {
         await axios.post("https://itoccupa-production.up.railway.app/api/form-submissions", {
+          submitted_at : formattedDate,
         });
 
         setProjectManagerScore(suitabilityPercentage.toFixed(2));
